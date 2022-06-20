5c0e898279082175e512d329320c5a63e480f633
Felipe Cardozo <fraifelipe@gmail.com>
2022-06-19 21:47:59 -0300

    change height of home component

--- frontend/src/components/tools/Home.tsx
+++ frontend/src/components/tools/Home.tsx
@@ -3,7 +3,7 @@ import React from 'react';
 function Home() {
   return (
     <div className='flex justify-center w-screen dark:bg-slate-500'>
-        <div className="bg-white dark:bg-slate-800 rounded-lg px-6 py-8 ring-1 ring-slate-900/5 shadow-xl w-11/12 h-1/6 mt-8">
+        <div className="bg-white dark:bg-slate-800 rounded-lg px-6 py-8 ring-1 ring-slate-900/5 shadow-xl w-11/12 h-max mt-8">
         <h1 className="text-slate-900 dark:text-white mt-0 text-9xl font-medium tracking-tight">Tz Tools</h1>
         <p className="text-slate-500 dark:text-slate-400 mt-2 text-lg">
             By Tezos developers, for Tezos developers.
