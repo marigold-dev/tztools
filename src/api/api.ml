3edd3b76c855b6c42013db5cc0bf26c8e6aa3d56
JulesGuesnon <jules.guesnon@hetic.net>
2022-09-14 18:35:30 +0200

    feat: Added documentation

--- src/api/api.ml
+++ src/api/api.ml
@@ -51,19 +51,44 @@ let ed25519_public request =
 
 let health _request = "Service is up and running." |> Dream.respond
 
+module Config = struct
+  type app = Dream.handler
+  type route = Dream.route
+  type handler = Dream.handler
+
+  let json_path = "/openapi.json"
+  let doc_path = "/docs"
+
+  let json_route json = Dream.get json_path (fun _ -> Dream.json json)
+
+  let doc_route html = Dream.get doc_path (fun _ -> Dream.html html)
+
+  let get = Dream.get
+  let post = Dream.post
+  let delete = Dream.delete
+  let put = Dream.put
+  let options = Dream.options
+  let head = Dream.head
+  let patch = Dream.patch
+
+  let build_routes = Dream.router
+end
+
+module OpenRouter = Openapi_router.Make (Config)
+
 let start () =
-  Dream.router
-    [
-      Dream.get "/base58/encode/:req" base58_encode;
-      Dream.get "/base58/decode/:req" base58_decode;
-      Dream.get "/block_hash/hash/:req" block_hash;
-      Dream.get "/blake2b/hash/:req" blake2b_hash;
-      Dream.get "/ed25519/secret_to_public/:req" ed25519_secret_to_public;
-      Dream.get "/ed25519/sign" ed25519_sign;
-      Dream.get "/ed25519/verify" ed25519_verify;
-      Dream.get "/ed25519/public" ed25519_public;
-      Dream.get "/health" health;
-    ]
+  OpenRouter.empty
+  |> OpenRouter.description "Tezos libraries as an api"
+  |> OpenRouter.get "/base58/encode/:req" base58_encode
+  |> OpenRouter.get "/base58/decode/:req" base58_decode
+  |> OpenRouter.get "/block_hash/hash/:req" block_hash
+  |> OpenRouter.get "/blake2b/hash/:req" blake2b_hash
+  |> OpenRouter.get "/ed25519/secret_to_public/:req" ed25519_secret_to_public
+  |> OpenRouter.get "/ed25519/sign" ed25519_sign
+  |> OpenRouter.get "/ed25519/verify" ed25519_verify
+  |> OpenRouter.get "/ed25519/public" ed25519_public
+  |> OpenRouter.get ~description:"Is service up or not" "/health" health
+  |> OpenRouter.build
   |> Middlewares.cors_middleware
   |> Dream.logger
   |> Dream.run ~interface:"0.0.0.0" ~port:8080
